5c9becaaa11edb7254c6e1632166945587812877
Miguel Ángel López Moyano <i02lomom@gmail.com>
2015-11-22 16:48:45 +0100

    Update ConfusingHideout.java
    Create class ConfusingHideout with the following statements. Interpret them and discuss them in the code.

--- ConfusingHideout.java
+++ ConfusingHideout.java
@@ -16,7 +16,7 @@ public class ConfusingHideout{
 			int x = 77;	/*I declare again and it is assigned a new value.
 					  You can not declare a variable again with the same
 					  name. We must remove int and just assign the new value
-					  or we change the name of the variable.*/
+					  or we change the name of the variable*/
 			}
 	}
 }
