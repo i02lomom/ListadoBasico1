1e8677663cd2a42889a5ba3c15979365e028b981
Miguel Ángel López Moyano <i02lomom@gmail.com>
2015-12-05 13:47:47 +0100

    Create CaracteresEspagnoles.java

---
+++ CaracteresEspagnoles.java
@@ -0,0 +1,16 @@
+/**
+ * Clase CaracteresEspagnoles
+ *
+ * Crea la clase CaracteresEspagnoles que muestre la siguiente salida.
+ * Para ello utiliza el método System.out.print() con los caracteres \n y \t.
+ * Utiliza el formato de carácter \udddd
+ * 
+ * @author Miguel Angel López Moyano
+ * @version 1.0
+ */
+class CaracteresEspagnoles{
+	public static void main(String[] args){
+		System.out.println("Estos caracteres son muy nuestros:");
+		System.out.println("\u00E1\t\u00ED\t\u00f3\t\u00fa\t\u00f1\t\u00d1\t\u00aa\t\u00ba");
+	}
+}
