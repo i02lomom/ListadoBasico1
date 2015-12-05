0c77c35437cd3032615c02abfae57cc6a4392d33
Miguel Ángel López Moyano <i02lomom@gmail.com>
2015-12-05 13:53:49 +0100

    Create EsconditeConfuso.java

---
+++ EsconditeConfuso.java
@@ -0,0 +1,23 @@
+/**
+ * Clase EsconditeConfuso
+ *
+ * Crea la clase EsconditeConfuso con las siguientes sentencias.
+ * Interprétalas y coméntalas en el código
+ * 
+ * @author Miguel Angel López Moyano
+ * @version 1.0
+ */
+public class EsconditeConfuso{
+	public static void main(String args[]){
+		
+		int x = 17;		//Se declara e inicializa la variable
+			{
+			int x = 77;	/*Se vuelve a declarar y se le asigna un 
+						nuevo valor. No se puede volver a declarar
+						una variable con el mismo nombre, o bien
+						quitamos el int y solo le asignamos el nuevo
+						valor o cambiamos el nombre de la variable
+						*/
+			}
+	}
+}
